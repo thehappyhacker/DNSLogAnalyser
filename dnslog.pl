fab5c43dc27dc74ca91ae36d3896a823a08a8634
thehappyhacker <laurian@ionescu.net>
2015-11-04 22:06:37 +0100

    Report is generated.

--- dnslog.pl
+++ dnslog.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use diagnostics;
 
 my %dns = ();
 
@@ -23,7 +24,7 @@ sub block_domain {
     $$tail_hash{$tail_key} = '*';
 }
 
-sub check_if_blocked {
+sub is_blocked {
     my ($domain) = @_;
     my $p = \%dns;
     for my $key(reverse(split(/\./, $domain))) {
@@ -38,17 +39,52 @@ sub check_if_blocked {
     }
 }
 
-block_domain('ad.net');
-block_domain('www.bad.net');
-block_domain('bad.net');
-
-
-
-my @doms = ('ad.net', '2.ad.net', 'good.net');
-for my $dom (@doms) {
-    if(check_if_blocked($dom)) {
-	print "$dom is blocked\n";
-    } else {
-	print "$dom is not blocked\n";
+sub process_hosts_blocked {
+    my($filename) = @_;
+    open my $host_blocked_file, '<', $filename or die "Could not open $filename\n";
+    
+    while (my $line = <$host_blocked_file> ) {
+	chomp($line);
+	block_domain($line);
     }
-} 
+}
+
+my %access = ();
+
+sub rev_dom {
+    my($domain) = @_;
+    return join('.', reverse(split(/\./, $domain)));
+}
+
+sub process_dns_query_log {
+    my($filename) = @_;
+    open my $dns_query_log_file, '<', $filename or die "Could not open $filename\n";
+    while (my $line = <$dns_query_log_file> ) {
+	my @words = split(/query:\s/, $line);
+	@words = split(/\s/, $words[1]);
+	my $domain = $words[0];
+	my @segments = split(/\./, $domain);
+	if($#segments gt 1) {
+	    #ignore domain names with no dot inside
+
+	    if(!is_blocked($domain)) {
+		if(exists($access{$domain})) {
+		    $access{$domain}++;
+		} else {
+		    $access{$domain} = 1;
+		}
+	    }
+	}
+    }
+    my @sorted_keys = sort {rev_dom($a) cmp rev_dom($b) } keys %access;
+    for my $key (@sorted_keys) {
+	print("$key $access{$key}\n");
+    }
+}
+
+
+my $hosts_blocked = $ARGV[0];
+my $dns_query_log = $ARGV[1];
+process_hosts_blocked($hosts_blocked);
+process_dns_query_log($dns_query_log);
+
