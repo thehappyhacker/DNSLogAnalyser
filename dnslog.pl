125b675b20e9e2ba55f31f99aec6a4377c7bdd3b
thehappyhacker <laurian@ionescu.net>
2015-11-07 00:19:53 +0100

    Can write back the simplified blocked list. Accepts two different commands.

--- dnslog.pl
+++ dnslog.pl
@@ -3,11 +3,14 @@ use strict;
 use warnings;
 #use diagnostics;
 #use re 'debug';
+use 5.10.0;
 
 my %dns = ();
 
 my $DOMAIN_REGEX = qr /(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])/o;
 my $EXTRACT_DOMAIN_REGEX = qr /query:\s(${DOMAIN_REGEX})\sIN\sA/o;
+
+my $COMMENT_REGEX = qr /^\s*#/o;
 
 sub block_domain {
     my ($domain) = @_;
@@ -51,9 +54,10 @@ sub process_hosts_blocked {
     
     while (my $line = <$host_blocked_file> ) {
 	chomp($line);
-	if($line =~ /$DOMAIN_REGEX/) {
-	    block_domain($line);
+	if($line =~ /$COMMENT_REGEX/) {
+	    next;
 	}
+	block_domain($line);
     }
 }
 
@@ -83,13 +87,52 @@ sub process_dns_query_log {
     }
     my @sorted_keys = sort {reverse_domain($a) cmp reverse_domain($b) } keys %access;
     for my $key (@sorted_keys) {
-	print("$key $access{$key}\n");
+	say "$key $access{$key}";
     }
 }
 
+sub serialize_blocked_domains_rec {
+    my($result, $tree, $path) = @_;
+    for my $key (keys %$tree) {
+	if($$tree{$key} eq '*') {
+	    push @$result, "$path.$key";
+	} else {
+	    serialize_blocked_domains_rec($result, $$tree{$key}, $path . $key);
+	}
+    }
+}
 
-my $hosts_blocked = $ARGV[0];
-my $dns_query_log = $ARGV[1];
-process_hosts_blocked($hosts_blocked);
-process_dns_query_log($dns_query_log);
+sub serialize_blocked_domains() {
 
+    my @block_list = ();
+    serialize_blocked_domains_rec(\@block_list, \%dns, '');
+    @block_list = sort(@block_list);
+
+    map $_ = reverse_domain($_), @block_list;
+
+    for my $dom (@block_list) {
+	say $dom;
+    }
+}
+
+sub main {
+    my $hosts_blocked = "hosts_blocked.txt";
+    my $dns_query_log = "";
+    my $command;
+    for my $e (@ARGV) {
+	given($e) {
+	    when (/\.txt$/i) { $hosts_blocked = $e; }
+	    when (/\.log$/i) { $dns_query_log = $e; }
+	    default { $command = $e; }
+	}
+    }
+
+    process_hosts_blocked($hosts_blocked);
+
+    given($command) {
+	when("processlog") { process_dns_query_log($dns_query_log); }
+	when("simplify") { serialize_blocked_domains(); }
+    }
+}
+
+main;
