c9efc641aa5f7acde4990387e3356691943c3c63
thehappyhacker <laurian@ionescu.net>
2015-11-07 20:54:48 +0100

    Implemented the add command.

--- dnslog.pl
+++ dnslog.pl
@@ -7,7 +7,8 @@ use 5.10.0;
 
 my %dns = ();
 
-my $DOMAIN_REGEX = qr /(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])/o;
+my $VALID_DOMAIN_REGEX = qr /((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*(([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-])*[A-Za-z0-9]))$/o;
+my $DOMAIN_REGEX =       qr /(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+(([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-])*[A-Za-z0-9])/o;
 my $EXTRACT_DOMAIN_REGEX = qr /query:\s(${DOMAIN_REGEX})\sIN\sA/o;
 
 my $COMMENT_REGEX = qr /^\s*#/o;
@@ -50,14 +51,11 @@ sub is_blocked {
 
 sub process_hosts_blocked {
     my($filename) = @_;
-    open my $host_blocked_file, '<', $filename or die "Could not open $filename\n";
+    open my $host_blocked_file, '<', $filename or die "Could not open blocked hosts file [$filename]\n";
     
     while (my $line = <$host_blocked_file> ) {
-	chomp($line);
-	if($line =~ /$COMMENT_REGEX/) {
-	    next;
-	}
-	block_domain($line);
+	$line =~ /$COMMENT_REGEX/ and next;
+	$line =~ /$VALID_DOMAIN_REGEX/ and block_domain($1);
     }
 }
 
@@ -70,7 +68,7 @@ sub reverse_domain {
 
 sub process_dns_query_log {
     my($filename) = @_;
-    open my $dns_query_log_file, '<', $filename or die "Could not open $filename\n";
+    open my $dns_query_log_file, '<', $filename or die "Could not open query log file [$filename]\n";
     while (my $line = <$dns_query_log_file> ) {
 	$line =~ /$EXTRACT_DOMAIN_REGEX/;
 	my $domain = $1;
@@ -93,11 +91,13 @@ sub process_dns_query_log {
 
 sub serialize_blocked_domains_rec {
     my($result, $tree, $path) = @_;
+    $path = $path && "$path.";
     for my $key (keys %$tree) {
+	$key or die "empty key in dns cache.\n";
 	if($$tree{$key} eq '*') {
-	    push @$result, "$path.$key";
+	    push @$result, "$path$key";
 	} else {
-	    serialize_blocked_domains_rec($result, $$tree{$key}, $path . $key);
+	    serialize_blocked_domains_rec($result, $$tree{$key}, "$path$key");
 	}
     }
 }
@@ -115,23 +115,49 @@ sub serialize_blocked_domains() {
     }
 }
 
+sub add_domain {
+    my (@domains) = @_;
+    for my $domain (@domains) {
+	$domain =~ /$VALID_DOMAIN_REGEX/ or die "Invalid domain name: [$domain]\n";
+	block_domain $domain;
+    }
+}
+
 sub main {
     my $hosts_blocked = "hosts_blocked.txt";
     my $dns_query_log = "";
     my $command;
+    my @params = ();
     for my $e (@ARGV) {
 	given($e) {
 	    when (/\.txt$/i) { $hosts_blocked = $e; }
 	    when (/\.log$/i) { $dns_query_log = $e; }
-	    default { $command = $e; }
+	    default { 
+		if(!$command) {
+		    $command = $e;
+		} else {
+		    push @params, $e;
+		}
+	    }
 	}
     }
 
     process_hosts_blocked($hosts_blocked);
 
     given($command) {
+	when ($_ eq "help" || $_ eq "?") { 
+	    say STDERR "  Usage:";
+	    say STDERR "    $0 <blocked_hosts_file.txt> <dnsquery.log> help|simplify|processlog|add";
+	    say "";
+	    say "    Files are identified by extension.";
+	    say "    The first non file parameter is the command. All followin parameters are command parameters."
+	}
 	when("processlog") { process_dns_query_log($dns_query_log); }
-	when("simplify") { serialize_blocked_domains(); }
+	when("simplify") { serialize_blocked_domains; }
+	when("add") {
+	    add_domain @params;
+	    serialize_blocked_domains;
+	}
     }
 }
 
